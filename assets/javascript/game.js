d0368d8cfe3b76fc2ab5c1f3083e2db20f7e9161
CLDavis15 <calebldavis0@gmail.com>
2019-02-09 13:37:21 -0500

    adding to the javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+var targetNumber = Math.floor((Math.random() * 120) + 19);
+
+        
+// the target number needed to win the game.
+
+$("#number-needed").text(targetNumber);
+
+var counter = 0;
+
+
+var numberOptions = Math.floor(Math.random() * )
