88c6582d3c54fe5e193e8094c445e6e4b2d3997d
CLDavis15 <calebldavis0@gmail.com>
2019-02-10 14:06:00 -0500

    working on functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,81 @@
+$(document).ready(function(){
 
-var targetNumber = Math.floor((Math.random() * 120) + 19);
+    
+    var targetNumber = Math.floor((Math.random() * 120) + 19);
+    console.log(targetNumber);
+    
+    // the target number needed to win the game.
+    
+    $("#number-needed").text(targetNumber);
+    
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
 
-        
-// the target number needed to win the game.
-
-$("#number-needed").text(targetNumber);
-
-var counter = 0;
+    $("#number-wins").text(wins);
+    $("#number-losses").text(losses);
 
 
-var numberOptions = Math.floor(Math.random() * )
+    var crystalValues = {};
+    crystalValues[1] = Math.floor(Math.random() * 12) + 1);
+    crystalValues[2] = Math.floor(Math.random() * 12) + 1);
+    crystalValues[3] = Math.floor(Math.random() * 12) + 1);
+    crystalValues[4] = Math.floor(Math.random() * 12) + 1);
+   
+    function reset() {
+        targetNumber = Math.floor((Math.random() * 120) + 19);
+
+        $("#number-needed").text(targetNumber);
+
+        var crystalValues = {};
+        crystalValues[1] = Math.floor(Math.random() * 12) + 1);
+        crystalValues[2] = Math.floor(Math.random() * 12) + 1);
+        crystalValues[3] = Math.floor(Math.random() * 12) + 1);
+        crystalValues[4] = Math.floor(Math.random() * 12) + 1);
+
+        userTotal = 0;
+
+        $("#total-score").text(userTotal);
+    }
+    
+    
+    function winner() {
+        wins++;
+        $("#number-wins").text(wins);
+
+        reset();
+    }
+    
+    function loser(){
+        losses++;
+        $("#number-losses").text(losses);
+        reset();
+    }
+    
+
+    function getCrystalTotal() {
+        return function(){
+            userTotal = userTotal + crystalValues[crystalKey];
+            console.log("New userTotal" + userTotal);
+            $("#total-score").text(userTotal);
+
+            if (userTotal === targetNumber) {
+                winner()
+            }
+            else if (userTotal >= targetNumber) {
+                loser()
+            }
+
+
+        }
+   
+    };
+    
+    $("#image1").on("click", getCrystalTotal(1));
+    $("#image2").on("click", getCrystalTotal(2));
+    $("#image3").on("click", getCrystalTotal(3));
+    $("#image4").on("click", getCrystalTotal(4));
+    
+
+    
+});
